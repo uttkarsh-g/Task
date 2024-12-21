f088961a5dd4629b5593fcbf8b39bac63b8e656b
Alexhawak <hloji999@gmail.com>
2024-12-21 15:37:36 +0530

    update

--- app.js
+++ app.js
@@ -1,27 +1,89 @@
-// const check = document.querySelectorAll('.checked');
-// const c = document.querySelectorAll('.userData');
-// const w = document.querySelector('.warning');
-// const submit = document.querySelector('#submit');
-// check.forEach((e) => {
-//   e.addEventListener('click', () => {
-//     const r = [...c].every((e) => {
-//       return e.value;
-//     });
-//     if (r) {
-//       e.parentElement.classList.add('completed');
-//     } else {
-//       w.classList.add('block');
-//     }
-//     const p = e.parentElement.classList.contains('completed');
-//     if (!p) {
-//       submit.disabled = true;
-//     } else {
-//       submit.disabled = false;
-//     }
-//     c.forEach((e) => {
-//       e.addEventListener('focus', () => {
-//         w.classList.remove('block');
-//       });
-//     });
-//   });
-// });
+const check = document.querySelectorAll('.checked');
+const c = document.querySelectorAll('.userData');
+const w = document.querySelector('.warning');
+const submit = document.querySelector('#submit');
+const resett = document.querySelector('#reset');
+const container = document.querySelector('.mainContent');
+submit.setAttribute('disabled', '');
+
+function reset() {
+  container.classList.remove('center');
+  container.innerHTML = `           <h2 id="today">Today</h2>
+          <p id="quotes">Raise the bar by completing your goals!</p>
+          <div id="progress">
+            <div id="progressbar">
+              <div id="progressupdate"></div>
+            </div>
+            <p class="warning">Please select all goals to continue</p>
+          </div>
+          <form action="" method="get">
+            <div class="goals">
+              <div class="checked"></div>
+              <input type="text" placeholder="Add new goal" class="userData" />
+            </div>
+            <div class="goals">
+              <div class="checked"></div>
+              <input type="text" placeholder="Add new goal" class="userData" />
+            </div>
+            <div class="goals">
+              <div class="checked"></div>
+              <input type="text" placeholder="Add new goal" class="userData" />
+            </div>
+          </form>
+          <div id="btn">
+            <button type="submit" id="submit">Submit</button>
+          </div>`;
+  running();
+}
+
+function running() {
+  const check = document.querySelectorAll('.checked');
+  const c = document.querySelectorAll('.userData');
+  const w = document.querySelector('.warning');
+  const submit = document.querySelector('#submit');
+  const resett = document.querySelector('#reset');
+  const container = document.querySelector('.mainContent');
+  submit.setAttribute('disabled', '');
+
+  check.forEach((element) => {
+    element.addEventListener('click', () => {
+      const r = [...c].every((e) => {
+        return e.value;
+      });
+      if (r) {
+        element.parentElement.classList.add('completed');
+        element.nextElementSibling.setAttribute('disabled', '');
+      } else {
+        w.classList.add('block');
+      }
+      const btnEbl = [...check].every((ele) =>
+        ele.parentElement.classList.contains('completed')
+      );
+
+      if (btnEbl) {
+        submit.removeAttribute('disabled');
+      }
+    });
+  });
+  c.forEach((focus) => {
+    focus.addEventListener('focus', () => {
+      w.classList.remove('block');
+    });
+  });
+  submit.addEventListener('click', (e) => {
+    e.stopPropagation();
+    container.classList.add('center');
+    container.innerHTML = ` <div class="celebrate">
+              <h2 id="celebTitle">
+                You Completed Your Task <span id="cTS"> Hurray!</span>
+              </h2>
+              <div class="button">
+                <h3>Set your task again -</h3>
+                <button id="reset">Taskly</button>
+              </div>
+            </div>`;
+    document.querySelector('#reset').addEventListener('click', reset);
+  });
+}
+
+running();
