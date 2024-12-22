d52b4ae6b8f64949435144a74a46791e78cc3f4e
Alexhawak <hloji999@gmail.com>
2024-12-22 16:51:05 +0530

    final update

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const check = document.querySelectorAll('.checked');
 const userData = document.querySelectorAll('.userData');
 const error = document.querySelector('.warning');
 const progressBar = document.querySelector('#progressupdate');
-const reset = document.querySelector('#reset');
+const container = document.querySelector('.mainContent');
+const submit = document.querySelector('#submit');
+submit.setAttribute('disabled', '');
+
 const userInputData = JSON.parse(localStorage.getItem('task')) || {};
 let width = Object.values(userInputData).filter((e) => e.isCompleted).length;
 progressBar.style.width = `${(width / 3) * 100}%`;
@@ -13,7 +16,7 @@ check.forEach((checkBtn) => {
       return input.value.trim();
     });
     if (userDataValue) {
-      checkBtn.parentElement.classList.toggle('completed');
+      checkBtn.parentElement.classList.add('completed');
       checkBtn.nextElementSibling.setAttribute('disabled', '');
       const nextElementId = checkBtn.nextElementSibling.id;
       userInputData[nextElementId].isCompleted =
@@ -25,6 +28,12 @@ check.forEach((checkBtn) => {
       localStorage.setItem('task', JSON.stringify(userInputData));
     } else {
       error.classList.add('block');
+    }
+    const btnEbl = [...check].every((ele) =>
+      ele.parentElement.classList.contains('completed')
+    );
+    if (btnEbl) {
+      submit.removeAttribute('disabled');
     }
   });
 });
@@ -45,8 +54,54 @@ userData.forEach((userDataInput) => {
     localStorage.setItem('task', JSON.stringify(userInputData));
   });
 });
+const btnEbl = [...check].every((ele) =>
+  ele.parentElement.classList.contains('completed')
+);
+if (btnEbl) {
+  submit.removeAttribute('disabled');
+}
 
-reset.addEventListener('click', () => {
-  localStorage.removeItem('task');
+function reset() {
+  container.classList.remove('center');
+  container.innerHTML = `           <h2 id="today">Today</h2>
+          <p id="quotes">Raise the bar by completing your goals!</p>
+          <div id="progress">
+            <div id="progressbar">
+              <div id="progressupdate"></div>
+            </div>
+            <p class="warning">Please select all goals to continue</p>
+          </div>
+          <form action="" method="get">
+            <div class="goals">
+              <div class="checked"></div>
+              <input type="text" placeholder="Add new goal" class="userData" />
+            </div>
+            <div class="goals">
+              <div class="checked"></div>
+              <input type="text" placeholder="Add new goal" class="userData" />
+            </div>
+            <div class="goals">
+              <div class="checked"></div>
+              <input type="text" placeholder="Add new goal" class="userData" />
+            </div>
+          </form>
+          <div id="btn">
+            <button type="submit" id="submit">Submit</button>
+          </div>`;
   location.reload();
+}
+submit.addEventListener('click', (e) => {
+  e.stopPropagation();
+  container.classList.add('center');
+  container.innerHTML = ` <div class="celebrate">
+            <h2 id="celebTitle">
+              You Completed Your Task <span id="cTS"> Hurray!</span>
+            </h2>
+            <div class="button">
+              <h3>Set your task again -</h3>
+              <button id="reset">Taskly</button>
+            </div>
+          </div>`;
+  localStorage.removeItem('task');
+  document.querySelector('#reset').addEventListener('click', reset);
 });
