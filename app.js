6b2fe5f5fa20ecf3511a8f20242e34d6dab162ea
Alexhawak <hloji999@gmail.com>
2024-12-22 16:27:04 +0530

    js update

--- app.js
+++ app.js
@@ -1,89 +1,52 @@
 const check = document.querySelectorAll('.checked');
-const c = document.querySelectorAll('.userData');
-const w = document.querySelector('.warning');
-const submit = document.querySelector('#submit');
-const resett = document.querySelector('#reset');
-const container = document.querySelector('.mainContent');
-submit.setAttribute('disabled', '');
+const userData = document.querySelectorAll('.userData');
+const error = document.querySelector('.warning');
+const progressBar = document.querySelector('#progressupdate');
+const reset = document.querySelector('#reset');
+const userInputData = JSON.parse(localStorage.getItem('task')) || {};
+let width = Object.values(userInputData).filter((e) => e.isCompleted).length;
+progressBar.style.width = `${(width / 3) * 100}%`;
 
-function reset() {
-  container.classList.remove('center');
-  container.innerHTML = `           <h2 id="today">Today</h2>
-          <p id="quotes">Raise the bar by completing your goals!</p>
-          <div id="progress">
-            <div id="progressbar">
-              <div id="progressupdate"></div>
-            </div>
-            <p class="warning">Please select all goals to continue</p>
-          </div>
-          <form action="" method="get">
-            <div class="goals">
-              <div class="checked"></div>
-              <input type="text" placeholder="Add new goal" class="userData" />
-            </div>
-            <div class="goals">
-              <div class="checked"></div>
-              <input type="text" placeholder="Add new goal" class="userData" />
-            </div>
-            <div class="goals">
-              <div class="checked"></div>
-              <input type="text" placeholder="Add new goal" class="userData" />
-            </div>
-          </form>
-          <div id="btn">
-            <button type="submit" id="submit">Submit</button>
-          </div>`;
-  running();
-}
-
-function running() {
-  const check = document.querySelectorAll('.checked');
-  const c = document.querySelectorAll('.userData');
-  const w = document.querySelector('.warning');
-  const submit = document.querySelector('#submit');
-  const resett = document.querySelector('#reset');
-  const container = document.querySelector('.mainContent');
-  submit.setAttribute('disabled', '');
-
-  check.forEach((element) => {
-    element.addEventListener('click', () => {
-      const r = [...c].every((e) => {
-        return e.value;
-      });
-      if (r) {
-        element.parentElement.classList.add('completed');
-        element.nextElementSibling.setAttribute('disabled', '');
-      } else {
-        w.classList.add('block');
-      }
-      const btnEbl = [...check].every((ele) =>
-        ele.parentElement.classList.contains('completed')
-      );
-
-      if (btnEbl) {
-        submit.removeAttribute('disabled');
-      }
+check.forEach((checkBtn) => {
+  checkBtn.addEventListener('click', () => {
+    const userDataValue = [...userData].every((input) => {
+      return input.value.trim();
     });
+    if (userDataValue) {
+      checkBtn.parentElement.classList.toggle('completed');
+      checkBtn.nextElementSibling.setAttribute('disabled', '');
+      const nextElementId = checkBtn.nextElementSibling.id;
+      userInputData[nextElementId].isCompleted =
+        !userInputData[nextElementId].isCompleted;
+      let width = Object.values(userInputData).filter(
+        (e) => e.isCompleted
+      ).length;
+      progressBar.style.width = `${(width / 3) * 100}%`;
+      localStorage.setItem('task', JSON.stringify(userInputData));
+    } else {
+      error.classList.add('block');
+    }
   });
-  c.forEach((focus) => {
-    focus.addEventListener('focus', () => {
-      w.classList.remove('block');
-    });
-  });
-  submit.addEventListener('click', (e) => {
-    e.stopPropagation();
-    container.classList.add('center');
-    container.innerHTML = ` <div class="celebrate">
-              <h2 id="celebTitle">
-                You Completed Your Task <span id="cTS"> Hurray!</span>
-              </h2>
-              <div class="button">
-                <h3>Set your task again -</h3>
-                <button id="reset">Taskly</button>
-              </div>
-            </div>`;
-    document.querySelector('#reset').addEventListener('click', reset);
-  });
-}
+});
 
-running();
+userData.forEach((userDataInput) => {
+  userDataInput.value = userInputData[userDataInput.id]?.task || '';
+  if (userInputData[userDataInput.id]?.isCompleted) {
+    userDataInput.parentElement.classList.add('completed');
+  }
+  userDataInput.addEventListener('focus', () => {
+    error.classList.remove('block');
+  });
+  userDataInput.addEventListener('input', () => {
+    userInputData[userDataInput.id] = {
+      task: userDataInput.value,
+      isCompleted: false,
+    };
+    localStorage.setItem('task', JSON.stringify(userInputData));
+  });
+});
+
+reset.addEventListener('click', () => {
+  localStorage.removeItem('task');
+  location.reload();
+});
